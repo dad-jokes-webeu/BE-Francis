c997c561e0a907dbf8e5cf853924454c31479c64
Francis Bulus <notchera@gmail.com>
2019-11-20 12:56:17 +0100

    dealt with white space

--- database/seeds/admin-user.js
+++ database/seeds/admin-user.js
@@ -3,7 +3,6 @@ exports.seed = function(knex) {
   return knex("users")
     .del()
     .then(function() {
-      // Inserts seed entries
       return knex("users").insert([
         { username: "dadjokesteam", email: "admin", password: "admin" }
       ]);
